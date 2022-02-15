4cdb05f6deda5ea9c28de587f46c9b884b4f6fdd
Berkan Sezer <berkan.sezer@useinsider.com>
2022-02-16 01:12:51 +0300

    Corne Magnet Configs Added

---
+++ keyboards/crkbd/keymaps/berkan/config.h
@@ -0,0 +1,35 @@
+
+
+#pragma once
+
+//#define USE_MATRIX_I2C
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define USE_SERIAL_PD2
+
+// #define TAPPING_FORCE_HOLD
+// #define TAPPING_TERM 100
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
+
+
+// Size optimisations
+#ifndef NO_DEBUG
+#define NO_DEBUG
+#endif // !NO_DEBUG
+#if !defined(NO_PRINT) && !defined(CONSOLE_ENABLE)
+#define NO_PRINT
+#endif // !NO_PRINT
+
+#define NO_ACTION_MACRO
+#define NO_ACTION_ONESHOT
+#define NO_ACTION_FUNCTION
+// Size optimisations
---
+++ keyboards/crkbd/keymaps/berkan/rules.mk
@@ -0,0 +1,11 @@
+OLED_ENABLE = no
+OLED_DRIVER = SSD1306
+# bongo cat
+WPM_ENABLE = no
+# bongo cat
+
+# Size optimisations
+MOUSEKEY_ENABLE = no
+# Size optimisations 
+
+EXTRAFLAGS += -flto
